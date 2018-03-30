2220902a09d55033b3b6be46c166ba80cde4e8df
Yuan Gao <gaoyuanbj@yahoo.com>
2018-03-29 22:15:04 -0400

    Bug

--- task_tracker/assets/js/components/tasks.jsx
+++ task_tracker/assets/js/components/tasks.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Tasks(params) {
   return (
     <p>test3</p>
+    <NavLink to="/tasks" exact={true} activeClassName="active" className="nav-link">
+      Tasks
+    </NavLink>
   );
 }
